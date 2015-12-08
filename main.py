1b07a1a11b78a808376d5a82923352a762a941ff
AxelTLarsson <mail@axellarsson.nu>
2015-12-08 16:16:42 +0100

    Incomplete: Continue impl of trajectory generator

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 import zmq
 from threading import Thread
+from scipy import interpolate
+from numpy import linspace
 
 
+COPTER_ID = 1
+LZ_ID = 2
+ABOUT_TRHESHOLD = 0.1
+HEIGHT = 0.5
+SET_POINTS = 10
+    
 class CrazyTrajectory(Thread):
+
+
+
 
     def __init__(self):
         Thread.__init__(self)
@@ -14,26 +25,72 @@ class CrazyTrajectory(Thread):
 
         self.controller_con = context.socket(zmq.PUSH)
         self.controller_con.connect('tcp://127.0.0.1:5124')
-        print(dir(self.camera_con))
+        self.copter_pos = None
+        self.lz_pos = None
 
     def run(self):
-        while True:
+        while not self.copter_pos and not self.lz_pos:
             data = self.camera_con.recv_json()
+            if data['id'] == COPTER_ID:
+                self.copter_pos = data
+            elif data['id'] == LZ_ID:
+                self.lz_pos = data
+            else:
+                print("Invalid id")
 
-    def _update_trajectory_curve():
+        curve = self._generate_trajectory_curve()
+        self.next_pos = next()
+        
+        while not self._is_at_lz():
+            data = self.camera_con.recv_json()
+            if data['id'] == COPTER_ID:
+                self.copter_pos = data
+            else:
+                continue
+            if self._is_at_pos(self.copter_pos, self.next_pos):
+                self.next_pos = curve.next()
+                self.controller_con.send_json({'set-points' : self.next_pos})
+
+    def _generate_trajectory_curve(self):
         """
         Calculates a curve between the current position and the target.
         """
-        return
+        start = self.copter_pos
+        end = self.lz_pos
+        mid = {'x': (start['x'] + end['x'])/2,
+               'y': (start['y'] + end['y'])/2,
+               'z': max(start['z'], end['z']) + HEIGHT}
 
-    def _get_target_pos(msg):
-        return {'x': 0, 'y': 0, 'y': 0}
+        x = [start[x], mid[x], end[x]]
+        y = [start[y], mid[y], end[y]]
+        z = [start[z], mid[z], end[z]]
 
-    def _get_current_pos(msg):
-        return {'x': 0, 'y': 0, 'y': 0}
+        (tck, u) = interpolate.splprep([x,y,z], k=2) 
+        t = linspace(0,1,SET_POINTS)
+        points = interpolate.splev(t, tck)
+        
+        for i in range(SET_POINTS):
+            x = points[0][i]
+            y = points[1][i]
+            z = points[2][i]
+            yield {'x': x, 'y': y, 'z': z}
 
-    def _is_at_pos(p1, p2):
-        return False
+    def _aboutEquals(self, a, b):
+        return abs(a - b) < ABOUT_TRHESHOLD
+
+    def _get_target_pos(self, msg):
+        return {'x': 0, 'y': 0, 'z': 0}
+
+    def _get_current_pos(self, msg):
+        return {'x': 0, 'y': 0, 'z': 0}
+
+    def _is_at_pos(self, p1, p2):
+        return  (self._aboutEquals(p1['x'], p2['x']) and
+                self._aboutEquals(p1['y'], p2['y']) and
+                self._aboutEquals(p1['z'], p2['z']))
+    
+    def _is_at_lz(self):
+        return  self._is_at_pos(self.copter_pos, self.lz_pos)
 
 trajectory = CrazyTrajectory()
 trajectory.start()
